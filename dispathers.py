bc570ce18fc0849aba7dc72c123c63f5570b8fc4
Евгений Зверков <evgenijzverkov@MBP-Evgenij.IGD_Rostelecom>
2024-09-11 19:58:41 +0700

    add stat

--- dispathers.py
+++ dispathers.py
@@ -13,7 +13,8 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     builder = ReplyKeyboardBuilder()
     builder.add(types.KeyboardButton(text='Начать игру'))
-    await message.answer(('Привет! Это квиз по вселенной "Властелин колец". Для старта введите "/quiz", либо нажмите "Начать игру"'), reply_markup=builder.as_markup())
+    builder.add(types.KeyboardButton(text='Статистика'))
+    await message.answer(('Привет! Это квиз по вселенной "Властелин колец". Для старта введите "/quiz", либо нажмите "Начать игру"'), reply_markup=builder.as_markup(resize_keyboard=True))
 
 
 async def get_question(message, user_id):
@@ -31,16 +32,6 @@ async def new_quiz(message):
     current_right_answers = 0
     await update_quiz_index(user_id, current_question_index, current_right_answers)
     await get_question(message, user_id)
-
-
-# async def get_quiz_index(user_id):
-#     async with aiosqlite.connect(DB_NAME) as db:
-#         async with db.execute('SELECT question_index FROM quiz_state WHERE user_id = (?)', (user_id, )) as cursor:
-#             result = await cursor.fetchone()
-#             if result is not None:
-#                 return result[0]
-#             else:
-#                 return 0
 
 
 async def get_data_from_db(user_id, data):
@@ -77,6 +68,13 @@ def generate_options_keyboard(answer_options, right_answer):
 async def cmd_quiz(message: types.Message):
     await message.answer('Давайте начнем квиз')
     await new_quiz(message)
+
+
+@dp.message(F.text == 'Статистика')
+@dp.message(Command("stat"))
+async def get_stat(message: types.Message):
+    current_right_answers = await get_data_from_db(message.from_user.id, 'right_answers')
+    await message.answer(f'Ваш последний результат: {current_right_answers} из {len(quiz_data)}')
 
 
 async def create_table():
